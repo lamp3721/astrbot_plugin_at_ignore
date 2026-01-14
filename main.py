b2bf2f14310225a08ff9fa289f2f2886d62f42df
lamp <2932349894@qq.com>
2026-01-14 22:24:05 +0800

    feat: 添加忽略@消息插件
    - 新增 astrbot_plugin_at_ignore 插件
    - 使用最高优先级拦截 @ 机器人的消息
    - 调用 event.stop_event() 阻止 LLM 处理

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
+"""
+AstrBot 插件 - 忽略 @ 消息
+当机器人被 @ 时，忽略该消息，不进行任何响应和 LLM 处理。
+"""
+from sys import maxsize
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
+import astrbot.api.message_components as Comp
+from astrbot.api import logger
+from astrbot.api.event import AstrMessageEvent, filter
+from astrbot.api.star import Context, Star, register
+
+
+@register(
+    "astrbot_plugin_at_ignore",
+    "lamp",
+    "忽略 @ 消息，bot 不会响应任何 @ 它的消息",
+    "1.0.0",
+    "https://github.com/lamp3721/astrbot_plugin_at_ignore",
+)
+class IgnoreAtPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
+        logger.info("忽略 @ 插件已加载")
 
-    async def initialize(self):
-        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+    @filter.event_message_type(filter.EventMessageType.ALL, priority=maxsize)
+    async def ignore_at_messages(self, event: AstrMessageEvent):
+        """
+        拦截所有 @ 机器人的消息并阻止后续处理。
+        使用最高优先级 (maxsize) 确保在其他处理器之前执行。
+        """
+        messages = event.get_messages()
+        bot_id = str(event.get_self_id())
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str # 用户发的纯文本消息字符串
-        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+        # 检查消息中是否有 @ 机器人
+        for message in messages:
+            if isinstance(message, Comp.At):
+                # 检查是否 @ 的是机器人自己
+                if str(message.qq) == bot_id:
+                    logger.debug(
+                        f"[忽略@] 拦截来自 {event.get_sender_name()}({event.get_sender_id()}) 的 @ 消息"
+                    )
+                    # 终止事件传播，阻止后续的 LLM 处理和其他响应
+                    event.stop_event()
+                    return
 
     async def terminate(self):
-        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+        """插件被卸载/停用时调用"""
+        logger.info("忽略 @ 插件已卸载")
